64c67a1c05a82d6441fde5a471dae9116a70d6db
romcar <rommel.carrasquilla@gmail.com>
2018-10-04 18:42:28 -0400

    Implement functionality that let's you open files in new notes

--- src/background.js
+++ src/background.js
@@ -24,6 +24,13 @@ if (process.platform === 'win32') {
   app.commandLine.appendSwitch('force-device-scale-factor', '1')
 }
 
+/**
+ * Creates a window of type view, the view gets added to the url we load. From
+ * there inside of the ViewManager component we then make use of this variable as
+ * a search query.
+ *
+ * @param {string} view type of component you want displayed.
+ */
 const createWindow = exports.createWindow = (view) => {
   // Create the browser window.
   let newWindow = new BrowserWindow({
@@ -65,9 +72,43 @@ const createWindow = exports.createWindow = (view) => {
   })
 }
 
-const getUserSelectedFilePath = () => {
+/**
+ * Uses the native showOpenDialog to ask the user for a file.@abstract
+ *
+ * @param {BrowserWindow} triggeringWindow The window that invoked the function.
+ * @returns {string} String with selected file path
+ */
+const getUserSelectedFilePath = exports.getUserSelectedFilePath = (triggeringWindow) => {
+  const files = dialog.showOpenDialog(triggeringWindow, {
+    properties: ['openFile'],
+    filters: [
+      { name: "All Files", extensions: ["*"] },
+      { name: "Markdown", extensions: ['md', 'markdown'] },
+      { name: "Text", extensions: ['txt', 'text'] },
+    ]
+  });
 
+  if (!files) return;
+
+  const file = files[0];
+  return file;
+};
+
+const getFileContents = exports.getFileContents = (event, filePath) => {
+  let content = fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      dialog.showMessageBox({
+        type: "error",
+        title: "Something happened",
+        message: `Something went wrong and this is what we found: ${err}`
+      });
+    }
+    event.sender.send('load-file-reply', data);
+  })
 }
+
+
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
@@ -91,3 +132,9 @@ app.on('activate', () => {
     createWindow()
   }
 })
+
+
+/*============================================================================================  */
+ipcMain.on('load-file', getFileContents);
+
+/*============================================================================================  */
--- src/containers/Note.jsx
+++ src/containers/Note.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
-
+import { remote, ipcRenderer } from 'electron';
 import Header from '../components/Header/Header';
+const mainProcess = remote.require('./background');
 export default class componentName extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      noteText: ""
+    }
+
+    /* Bindings */
+    this.handleFileOpen = this.handleFileOpen.bind(this);
+    this.handleFileLoad = this.handleFileLoad.bind(this);
+    this.handleNoteTextChange = this.handleNoteTextChange.bind(this);
 
     this.menuOptions = [
       {
@@ -15,7 +25,7 @@ export default class componentName extends Component {
       {
         action: 'Open',
         icon: 'far fa-folder-open fa-lg',
-        cb: () => { }
+        cb: () => this.handleFileOpen()
       },
       {
         action: 'Save',
@@ -25,12 +35,48 @@ export default class componentName extends Component {
 
 
     ];
+
   }
+
+  componentDidMount() {
+    ipcRenderer.on('load-file-reply', this.handleFileLoad);
+  }
+
+  componentWillUnmount() {
+    ipcRenderer.removeEventListener('load-file-reply');
+  }
+
+  handleFileLoad(event, contents) {
+    console.log(contents);
+    this.setState({ noteText: contents });
+    // const nodeTextElement = document.getElementsByClassName('note--text')[0];
+    // nodeTextElement.value = contents;
+  }
+
+  handleNoteTextChange(e) {
+    let { value } = e.target;
+    this.setState({ noteText: value });
+  }
+
+  handleFileOpen() {
+    // console.log('click');
+
+    let currentWindow = remote.currentWindow;
+    let filePath = mainProcess.getUserSelectedFilePath(currentWindow);
+
+    let title = document.getElementsByClassName('header--title')[0];
+    title.innerHTML = filePath;
+    ipcRenderer.send('load-file', filePath);
+  }
+
   render() {
     return (
       <div className="grid note">
         <Header menuOptions={this.menuOptions} view={this.props.view} />
-        <textarea className="note--text"></textarea>
+        <textarea
+          onChange={this.handleNoteTextChange}
+          value={this.state.noteText}
+          className="note--text"></textarea>
       </div>
     )
   }
